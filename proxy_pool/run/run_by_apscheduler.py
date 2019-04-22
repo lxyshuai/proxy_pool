a0c6e5ba87d75f04f825867c157865d60a02438d
lxyshuai <442536013@qq.com>
2019-04-22 20:21:02 +0800

    apscheduler多线程启动模式添加flask api

--- proxy_pool/run/run_by_apscheduler.py
+++ proxy_pool/run/run_by_apscheduler.py
@@ -2,6 +2,7 @@ import datetime
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+from proxy_pool.api.proxy_api import api_run
 from proxy_pool.proxy_getter.proxy_fetch import ProxyFetch
 from proxy_pool.validator.raw_proxy_check import RawProxyCheck
 from proxy_pool.validator.valid_proxy_check import ValidProxyCheck
@@ -15,4 +16,5 @@ if __name__ == '__main__':
     scheduler.add_job(ProxyFetch.call_all_proxy_getter, "interval", minutes=10,
                       next_run_time=datetime.datetime.now(),
                       id="proxy_fetch")
+    scheduler.add_job(api_run, id='api_run')
     scheduler.start()
