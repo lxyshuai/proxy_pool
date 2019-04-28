4aadc4b3363800b0bb18db9a261ed3eb4bc80ff1
lxyshuai <442536013@qq.com>
2019-04-28 19:37:11 +0800

    修改Api，使用restful风格的API

--- proxy_pool/api/proxy_api.py
+++ proxy_pool/api/proxy_api.py
@@ -1,59 +1,67 @@
 import json
 
 from flask import Flask
+from flask_restful import Resource, Api
 
 from proxy_pool.config import settings
 from proxy_pool.db import Client
 from proxy_pool.db.model.proxy import HTTP_PROTOCOL, HTTPS_PROTOCOL
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route("/get_http_proxy/")
-def get_http_proxy():
-    condition_dict = {
-        "is_valid": True,
-        "protocol": HTTP_PROTOCOL
-    }
-    proxy_list = Client.select(1, condition_dict)
-    if proxy_list:
-        proxy = Client.select(1, condition_dict)[0]
-        result_dict = {
-            "message": "Get a http proxy successfully",
-            "ip": proxy.ip,
-            "port": proxy.port,
-            "protocol": "http"
+class HttpProxy(Resource):
+    def get(self):
+        condition_dict = {
+            "is_valid": True,
+            "protocol": HTTP_PROTOCOL
         }
-    else:
-        result_dict = {
-            "message": "Get a http proxy unsuccessfully",
-        }
-    result = json.dumps(result_dict)
-    return result
+        proxy_list = Client.select(1, condition_dict)
+        if proxy_list:
+            proxy = Client.select(1, condition_dict)[0]
+            result_dict = {
+                "message": "Get a http proxy successfully",
+                "ip": proxy.ip,
+                "port": proxy.port,
+                "protocol": "http"
+            }
+        else:
+            result_dict = {
+                "message": "Get a http proxy unsuccessfully",
+            }
+        return result_dict
 
 
-@app.route("/get_https_proxy/")
-def get_https_proxy():
-    condition_dict = {
-        "is_valid": True,
-        "protocol": HTTPS_PROTOCOL
-    }
-    proxy_list = Client.select(1, condition_dict)
-    if proxy_list:
-        proxy = Client.select(1, condition_dict)[0]
-        result_dict = {
-            "message": "Get a https proxy successfully",
-            "ip": proxy.ip,
-            "port": proxy.port,
-            "protocol": "https"
+class HttpsProxy(Resource):
+    def get(self):
+        condition_dict = {
+            "is_valid": True,
+            "protocol": HTTPS_PROTOCOL
         }
-    else:
-        result_dict = {
-            "message": "Get a https proxy unsuccessfully",
-        }
-    result = json.dumps(result_dict)
-    return result
+        proxy_list = Client.select(1, condition_dict)
+        if proxy_list:
+            proxy = Client.select(1, condition_dict)[0]
+            result_dict = {
+                "message": "Get a https proxy successfully",
+                "ip": proxy.ip,
+                "port": proxy.port,
+                "protocol": "https"
+            }
+        else:
+            result_dict = {
+                "message": "Get a https proxy unsuccessfully",
+            }
+        return result_dict
+
+
+api.add_resource(HttpProxy, '/http_proxy')
+api.add_resource(HttpsProxy, '/https_proxy')
 
 
 def api_run():
-    host = settings.SERVER_API['host'], port = settings.SERVER_API['port']
+    app.run(host=settings.SERVER_API['HOST'], port=settings.SERVER_API['PORT'])
+
+
+if __name__ == '__main__':
+    app.run(host=settings.SERVER_API['HOST'], port=settings.SERVER_API['PORT'])
