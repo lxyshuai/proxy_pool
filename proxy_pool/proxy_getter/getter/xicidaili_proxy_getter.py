e79fd77ce5c11f70b5106c04e95c3ffa877147af
lxyshuai <442536013@qq.com>
2019-05-04 15:01:24 +0800

    添加代理获取模块爬虫的异常处理，一个爬虫失败直接跳出进行下一个爬虫

--- proxy_pool/proxy_getter/getter/xicidaili_proxy_getter.py
+++ proxy_pool/proxy_getter/getter/xicidaili_proxy_getter.py
@@ -23,50 +23,56 @@ class XicidailiProxyGetter(ProxyGetter):
         """
         request = HtmlDownloader()
         proxy_getter_logger.info("Start get Xicidaili proxy")
-        # 爬取透明proxy
-        for page in range(1, 5):
-            # 拼接url
-            url = cls.transparent_url + str(page)
-            html = request.download(url=url)
-            # 从html中提取proxy
-            soup = BeautifulSoup(html, features="html.parser")
-            table = soup.find("table", id="ip_list")
-            for tr in table.find_all("tr")[1:]:
-                td_list = tr.find_all("td")
-                proxy_dict = {
-                    "country": "China",
-                    "ip": unicode(td_list[1].string),
-                    "port": int(td_list[2].string),
-                    "area": unicode(td_list[3].find("a").string if td_list[3].find("a") else ""),
-                    "type": TRANSPARENT,
-                    "protocol": HTTPS_PROTOCOL if td_list[5].string == "HTTPS" else HTTP_PROTOCOL,
-                    "is_valid": False
-                }
-                proxy_getter_logger.info(proxy_dict)
-                Client.insert(proxy_dict)
-            # 防止被代理网站反爬，添加间隔时间2s
-            time.sleep(1)
+        try:
+            # 爬取透明proxy
+            for page in range(1, 3):
+                # 拼接url
+                url = cls.transparent_url + str(page)
+                proxy_getter_logger.info("get url %s" % url)
+                html = request.download(url=url)
+                # 从html中提取proxy
+                soup = BeautifulSoup(html, features="html.parser")
+                table = soup.find("table", id="ip_list")
+                for tr in table.find_all("tr")[1:]:
+                    td_list = tr.find_all("td")
+                    proxy_dict = {
+                        "country": "China",
+                        "ip": unicode(td_list[1].string),
+                        "port": int(td_list[2].string),
+                        "area": unicode(td_list[3].find("a").string if td_list[3].find("a") else ""),
+                        "type": TRANSPARENT,
+                        "protocol": HTTPS_PROTOCOL if td_list[5].string == "HTTPS" else HTTP_PROTOCOL,
+                        "is_valid": False
+                    }
+                    proxy_getter_logger.info(proxy_dict)
+                    Client.insert(proxy_dict)
+                # 防止被代理网站反爬，添加间隔时间2s
+                time.sleep(2)
 
-        # 爬取高匿proxy
-        for page in range(1, 5):
-            # 拼接url
-            url = cls.anonymous_url + str(page)
-            html = request.download(url=url)
-            # 从html中提取proxy
-            soup = BeautifulSoup(html, features="html.parser")
-            table = soup.find("table", id="ip_list")
-            for tr in table.find_all("tr")[1:]:
-                td_list = tr.find_all("td")
-                proxy_dict = {
-                    "country": "China",
-                    "ip": unicode(td_list[1].string),
-                    "port": int(td_list[2].string),
-                    "area": unicode(td_list[3].find("a").string if td_list[3].find("a") else ""),
-                    "type": ANONYMOUS,
-                    "protocol": HTTPS_PROTOCOL if td_list[5].string == "HTTPS" else HTTP_PROTOCOL,
-                    "is_valid": False
-                }
-                proxy_getter_logger.info(proxy_dict)
-                Client.insert(proxy_dict)
-            # 防止被代理网站反爬，添加间隔时间2s
-            time.sleep(1)
+            # 爬取高匿proxy
+            for page in range(1, 3):
+                # 拼接url
+                url = cls.anonymous_url + str(page)
+                proxy_getter_logger.info("get url %s" % url)
+                html = request.download(url=url)
+                # 从html中提取proxy
+                soup = BeautifulSoup(html, features="html.parser")
+                table = soup.find("table", id="ip_list")
+                for tr in table.find_all("tr")[1:]:
+                    td_list = tr.find_all("td")
+                    proxy_dict = {
+                        "country": "China",
+                        "ip": unicode(td_list[1].string),
+                        "port": int(td_list[2].string),
+                        "area": unicode(td_list[3].find("a").string if td_list[3].find("a") else ""),
+                        "type": ANONYMOUS,
+                        "protocol": HTTPS_PROTOCOL if td_list[5].string == "HTTPS" else HTTP_PROTOCOL,
+                        "is_valid": False
+                    }
+                    proxy_getter_logger.info(proxy_dict)
+                    Client.insert(proxy_dict)
+                # 防止被代理网站反爬，添加间隔时间2s
+                time.sleep(2)
+            proxy_getter_logger.info("End get Xicidaili proxy successfully")
+        except Exception as e:
+            proxy_getter_logger.warn("End get Xicidaili proxy unsuccessfully, because %s" % e)
