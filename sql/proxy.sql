babb822578339c152e8a1891ccc328ffe24a0c51
lxyshuai <442536013@qq.com>
2019-05-04 14:52:32 +0800

    修改创建数据库文件，ip从varchar变为int

--- sql/proxy.sql
+++ sql/proxy.sql
@@ -1,3 +1,19 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 50724
+ Source Host           : localhost:3306
+ Source Schema         : proxy
+
+ Target Server Type    : MySQL
+ Target Server Version : 50724
+ File Encoding         : 65001
+
+ Date: 30/04/2019 18:23:03
+*/
+
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
@@ -7,7 +23,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `proxy`;
 CREATE TABLE `proxy`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ip` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `ip` int(10) UNSIGNED NOT NULL,
   `port` int(11) NOT NULL,
   `type` int(11) NOT NULL,
   `protocol` int(11) NOT NULL,
@@ -16,6 +32,6 @@ CREATE TABLE `proxy`  (
   `is_valid` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `unique_protocol://ip:port`(`protocol`, `ip`, `port`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 802 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
