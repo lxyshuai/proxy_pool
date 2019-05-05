90d28a30c257567055167d79c6ac2752ea568727
lxyshuai <442536013@qq.com>
2019-05-05 20:21:25 +0800

    添加apscheduler的日志设置，将proxy_check日志分为raw_proxy_check和valid_proxy_check

--- proxy_pool/utils/logger.py
+++ proxy_pool/utils/logger.py
@@ -6,6 +6,15 @@ CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
 ROOT_PATH = os.path.join(CURRENT_PATH, os.pardir + os.path.sep + os.pardir)
 LOG_PATH = os.path.join(ROOT_PATH, 'log')
 
+# apscheduler logger
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+    datafmt='%a, %d %b %Y %H:%M:%S',
+    filename=os.path.join(LOG_PATH, "apscheduler.log"),
+    filemode='a'
+)
+
 # db logger
 # 创建名为"db"的记录器
 db_logger = logging.getLogger("db")
@@ -42,12 +51,12 @@ console_handler.setFormatter(formatter)
 proxy_getter_logger.addHandler(file_handler)
 proxy_getter_logger.addHandler(console_handler)
 
-# proxy_check logger
+# raw_proxy_check logger
 # 创建名为"proxy_check_logger"的记录器
-proxy_check_logger = logging.getLogger("proxy_check")
-proxy_check_logger.setLevel(logging.DEBUG)
+raw_proxy_check_logger = logging.getLogger("raw_proxy_check")
+raw_proxy_check_logger.setLevel(logging.DEBUG)
 # 创建级别为DEBUG的日志文件处理器
-file_handler = logging.FileHandler(os.path.join(LOG_PATH, "proxy_check.log"))
+file_handler = logging.FileHandler(os.path.join(LOG_PATH, "raw_proxy_check.log"))
 file_handler.setLevel(logging.DEBUG)
 # 创建级别为DEBUG的控制台日志处理器
 console_handler = logging.StreamHandler()
@@ -58,5 +67,24 @@ formatter = logging.Formatter(
 file_handler.setFormatter(formatter)
 console_handler.setFormatter(formatter)
 # 将文件处理器和控制台处理器加入记录器
-proxy_check_logger.addHandler(file_handler)
-proxy_check_logger.addHandler(console_handler)
+raw_proxy_check_logger.addHandler(file_handler)
+raw_proxy_check_logger.addHandler(console_handler)
+
+# valid_proxy_check logger
+# 创建名为"valid_proxy_check_logger"的记录器
+valid_proxy_check_logger = logging.getLogger("valid_proxy_check")
+valid_proxy_check_logger.setLevel(logging.DEBUG)
+# 创建级别为DEBUG的日志文件处理器
+file_handler = logging.FileHandler(os.path.join(LOG_PATH, "valid_proxy_check.log"))
+file_handler.setLevel(logging.DEBUG)
+# 创建级别为DEBUG的控制台日志处理器
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+# 创建格式器，加到日志处理器和控制台处理器中
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(lineno)d - %(module)s - %(thread)d - %(message)s")
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+# 将文件处理器和控制台处理器加入记录器
+valid_proxy_check_logger.addHandler(file_handler)
+valid_proxy_check_logger.addHandler(console_handler)
