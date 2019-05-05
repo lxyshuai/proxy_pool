7f6af4bec4e8834a6b7d4e153589f567506f522d
lxyshuai <442536013@qq.com>
2019-05-05 20:19:35 +0800

    调整apscheduler中各个周期任务运行间隔

--- proxy_pool/run/run_apscheduler.py
+++ proxy_pool/run/run_apscheduler.py
@@ -1,6 +1,7 @@
 import datetime
 
 from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.interval import IntervalTrigger
 
 from proxy_pool.proxy_getter.proxy_fetch import ProxyFetch
 from proxy_pool.validator.raw_proxy_check import RawProxyCheck
@@ -9,12 +10,15 @@ from proxy_pool.validator.valid_proxy_check import ValidProxyCheck
 
 def run_apscheduler():
     scheduler = BlockingScheduler()
-    scheduler.add_job(ValidProxyCheck().check_valid_proxy, "interval", seconds=10,
+    scheduler.add_job(func=ValidProxyCheck().check_valid_proxy,
+                      trigger=IntervalTrigger(seconds=10),
                       next_run_time=datetime.datetime.now(),
                       id="valid_proxy_check")
-    scheduler.add_job(RawProxyCheck().check_raw_proxy, "interval", seconds=20,
+    scheduler.add_job(func=RawProxyCheck().check_raw_proxy,
+                      trigger=IntervalTrigger(seconds=10),
                       id="raw_proxy_check")
-    scheduler.add_job(ProxyFetch.call_all_proxy_getter, "interval", minutes=5,
+    scheduler.add_job(ProxyFetch.call_all_proxy_getter,
+                      trigger=IntervalTrigger(minutes=5),
                       next_run_time=datetime.datetime.now(),
                       id="proxy_fetch")
     scheduler.start()
