1e004223ee10c0376fc65859aabbdbd6e6997489
lxyshuai <442536013@qq.com>
2019-06-03 16:00:13 +0800

    data5u网页HTML结构变动，修改data5u代理源爬虫代码

--- proxy_pool/proxy_getter/getter/data5u_proxy_getter.py
+++ proxy_pool/proxy_getter/getter/data5u_proxy_getter.py
@@ -11,10 +11,7 @@ from proxy_pool.utils.logger import proxy_getter_logger
 
 
 class Data5uProxyGetter(ProxyGetter):
-    # 透明URL
-    transparent_url = 'http://www.data5u.com/free/gnpt/index.shtml'
-    # 高匿URL
-    anonymous_url = 'http://www.data5u.com/free/gngn/index.shtml'
+    url = 'http://www.data5u.com/'
 
     @classmethod
     def get_proxy(cls):
@@ -22,11 +19,9 @@ class Data5uProxyGetter(ProxyGetter):
         爬取无忧代理网站html，解析后获得代理将代理加入数据库
         """
         request = HtmlDownloader()
-        proxy_getter_logger.info("Start get Xicidaili proxy")
+        proxy_getter_logger.info("Start get data5u proxy")
         try:
-            # 爬取透明proxy
-            html = request.download(url=cls.transparent_url)
-            # 从html中提取proxy
+            html = request.download(url=cls.url)
             soup = BeautifulSoup(html, features="html.parser")
             ul_list = soup.find_all("ul", class_="l2")
             for ul in ul_list:
@@ -36,26 +31,7 @@ class Data5uProxyGetter(ProxyGetter):
                     "ip": unicode(li_list[0].string),
                     "port": int(li_list[1].string),
                     "area": unicode(li_list[5].string),
-                    "type": TRANSPARENT,
-                    "protocol": HTTPS_PROTOCOL if li_list[2].string == "https" else HTTP_PROTOCOL,
-                    "is_valid": False
-                }
-                proxy_getter_logger.info(proxy_dict)
-                Client.insert(proxy_dict)
-
-            # 爬取高匿proxy
-            html = request.download(url=cls.anonymous_url)
-            # 从html中提取proxy
-            soup = BeautifulSoup(html, features="html.parser")
-            ul_list = soup.find_all("ul", class_="l2")
-            for ul in ul_list:
-                li_list = ul.find_all("li")
-                proxy_dict = {
-                    "country": "China",
-                    "ip": unicode(li_list[0].string),
-                    "port": int(li_list[1].string),
-                    "area": unicode(li_list[5].string),
-                    "type": ANONYMOUS,
+                    "type": TRANSPARENT if li_list[2].string == u"透明" else ANONYMOUS,
                     "protocol": HTTPS_PROTOCOL if li_list[2].string == "https" else HTTP_PROTOCOL,
                     "is_valid": False
                 }
